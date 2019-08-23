dcef971ee35fcab2595dc5adeca97cec3551589d
hp <hp@192.168.10.109>
2019-08-23 14:01:39 +0200

    Testing Commit

---
+++ BegginnersGuide/src/Example2.java
@@ -0,0 +1,21 @@
+/*
+ * This demonstrates a variable
+ * 
+ * Call this file example2.java
+ */
+public class Example2 {
+	public static void main(String[] args) {
+		int var1; // this declares a variable <------Declare variables
+		int var2; // this declares another variable
+		
+		var1 = 1024; // this assigns 1024 to var1 <------Assign a variable a value
+		
+		System.out.println("var1 contains " + var1);
+		
+		var2 = var1 / 2;
+		
+		System.out.print("var2 contains var1 / 2: ");
+		System.out.println(var2);
+	}
+
+}
